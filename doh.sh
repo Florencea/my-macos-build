68b2ed8178617ce87cebc1cef6266a5719ed5605
Florencea <bearflorencea@gmail.com>
2019-02-17 16:03:42 +0800

    fix(doh.sh): remove incorrect doh server

--- doh.sh
+++ doh.sh
@@ -7,9 +7,6 @@ proxy-dns: true
 proxy-dns-upstream:
  - https://1.1.1.1/dns-query
  - https://1.0.0.1/dns-query
- - https://8.8.8.8/dns-query
- - https://8.8.4.4/dns-query
 EOF
 sudo cloudflared service install
 sudo launchctl start com.cloudflare.cloudflared
-networksetup -setdnsservers Wi-Fi 127.0.0.1
