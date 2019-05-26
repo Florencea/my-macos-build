2958f99d00e2f5f57f91eb3e0a72d9243a5b914f
Florencea <bearflorencea@gmail.com>
2019-05-26 14:20:41 +0800

    feat: add clone-all.sh

---
+++ clone-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+repo_list=$(curl -s https://api.github.com/user/repos?type=owner\&access_token=$1 | jq .[].ssh_url | sed -e 's/^"//'  -e 's/"$//')
+for repo in $repo_list
+do
+  git clone $repo
+done
