f969dd0aa662e7ebac1639ff87898a0ca99c6f8a
Florencea <bearflorencea@gmail.com>
2019-06-25 20:08:51 +0800

    feat: update brew list

--- install_full.sh
+++ install_full.sh
@@ -60,6 +60,7 @@ mkdir -p ~/.config/fish
 print_step "brew install commend line tools"
 brew install clang-format
 brew install ccls
+brew install ffmpeg
 brew install gcc
 brew install git
 brew install git-lfs
