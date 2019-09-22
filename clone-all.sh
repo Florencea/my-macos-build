4fa3d9f453073fd6acc6097f9edaa59ab082ece9
Florencea <bearflorencea@gmail.com>
2019-09-22 13:15:31 +0800

    feat: update clone-all.sh

--- clone-all.sh
+++ clone-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo_list=$(curl -s https://api.github.com/user/repos?type=owner\&access_token=$1 | jq .[].ssh_url | sed -e 's/^"//'  -e 's/"$//')
+repo_list=$(curl -s https://api.github.com/user/repos?type=owner\&per_page=1000\&access_token=$1 | jq .[].ssh_url | sed -e 's/^"//'  -e 's/"$//')
 for repo in $repo_list
 do
   git clone $repo
