2e6c2d9e28a153f0e3207c79493d49a15ca3c883
Florencea <bearflorencea@gmail.com>
2019-10-11 14:54:48 +0800

    fix: change fish to sh

--- install.sh
+++ install.sh
@@ -21,8 +21,8 @@ mkdir -p ~/.config/fish
 printf "set -g -x PATH /usr/local/bin \$PATH\n" >> ~/.config/fish/config.fish
 printf "set -g -x fish_greeting %s\n" "$shell_greeting" >> ~/.config/fish/config.fish
 printf "alias mmb=\"atom ~/GitHub/my-macos-build\"\n" >> ~/.config/fish/config.fish
-printf "alias mkgif=\"fish ~/GitHub/my-macos-build/make-gif.sh\"\n" >> ~/.config/fish/config.fish
-printf "alias ubk=\"fish ~/GitHub/my-macos-build/ublock-backup.sh\"\n" >> ~/.config/fish/config.fish
+printf "alias mkgif=\"sh ~/GitHub/my-macos-build/make-gif.sh\"\n" >> ~/.config/fish/config.fish
+printf "alias ubk=\"sh ~/GitHub/my-macos-build/ublock-backup.sh\"\n" >> ~/.config/fish/config.fish
 printf "alias al=\"fish ~/GitHub/ledger/al/al.fish\"\n" >> ~/.config/fish/config.fish
 
 print_step "brew install commend line tools"
