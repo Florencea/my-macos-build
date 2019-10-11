325652911a6ea0c972f80e2e9251ae74e2179c80
Florencea <bearflorencea@gmail.com>
2019-10-11 13:51:55 +0800

    feat: port mkgif.sh to zsh

--- make-gif.sh
+++ make-gif.sh
@@ -1,22 +1,23 @@
-#! /usr/bin/fish
-# require ffmpeg, fish
+#!/usr/bin/env zsh
+# require ffmpeg
 # nano ~/.config/fish/config.fish
-# alias mkgif="fish ~/Documents/workspace_florencea/my-macos-build/make-gif.sh"
-if test (count $argv) -lt 3; or test (count $argv) -gt 3
-echo "Usage: mkgif [input_file] [from(hh:mm:ss or sec)] [during(sec)]"
-echo "       mkgif \"input.mp4\" 01:02:08 11.0"
-exit 1
-end
-if test -f $argv[1]
-set n mkgif_(date +"%Y%m%d%H%M%S")
-set er (ffmpeg -hide_banner -loglevel error -ss $argv[2] -t $argv[3] -i "$argv[1]" -filter_complex "[0:v] fps=12,scale=w=480:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" "$n.gif" 2>&1)
-if test $er
-echo $er
-rm $n.gif > /dev/null 2>&1
-exit 1
+# alias mkgif="sh ~/GitHub/my-macos-build/make-gif.sh"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: mkgif [input_file] [from(hh:mm:ss or sec)] [during(sec)]"
+  echo "       mkgif \"input.mp4\" 01:02:08 11.0"
+  exit 1
+fi
+INPUT_FILE=$1
+OUTPUT_FILE="mkgif_$(date +"%Y%m%d%H%M%S").gif"
+if [ -f "$INPUT_FILE" ]; then
+  OUTPUT_ERROR=$(ffmpeg -hide_banner -loglevel error -ss $2 -t $3 -i "$1" -filter_complex "[0:v] fps=12,scale=w=480:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" "$OUTPUT_FILE" 2>&1)
+  if [ -n "$OUTPUT_ERROR" ]; then
+    echo $OUTPUT_ERROR
+    rm "$OUTPUT_FILE" > /dev/null 2>&1
+    exit 1
+  else
+    echo "done. see $OUTPUT_FILE"
+  fi
 else
-echo "done. see $n.gif"
-end
-else
-echo "file: \"$argv[1]\" do not exist."
-end
+  echo "file: \"$INPUT_FILE\" do not exist."
+fi
