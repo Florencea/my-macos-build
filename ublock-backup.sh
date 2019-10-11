5ab169137137e9f4328fa05859ba083cd1e4cb7b
Florencea <bearflorencea@gmail.com>
2019-10-11 14:17:43 +0800

    feat: port ubk to zsh

--- ublock-backup.sh
+++ ublock-backup.sh
@@ -1,19 +1,20 @@
-#! /usr/bin/fish
-# require fish
+#!/usr/bin/env zsh
 # nano ~/.config/fish/config.fish
-# alias ubk="fish ~/Documents/workspace_florencea/my-macos-build/ublock-backup.sh"
+# alias ubk="sh ~/GitHub/my-macos-build/ublock-backup.sh"
 cd ~/Downloads
-set file_to_backup (ls -t | grep my-ublock-backup | head -n1)
-if test "$file_to_backup" = ""
-  echo "Error: No ublock backups in ~/Downloads"
-else
-  mv $file_to_backup ~/Documents/workspace_florencea/my-macos-build/ublock-advanced.txt
-  cd ~/Documents/workspace_florencea/my-macos-build
-  git add ublock-advanced.txt
+FILE_TO_BACKUP=$(ls -t | grep my-ublock-backup | head -n1)
+FILE_NAME=ublock-advanced.txt
+PROJECT_DIR=~/GitHub/my-macos-build/
+if [ -f "$FILE_TO_BACKUP" ]; then
+  mv $FILE_TO_BACKUP $PROJECT_DIR$FILE_NAME
+  cd $PROJECT_DIR
+  git add $FILE_NAME
   echo ""
   printf 'Backup to github...'
   git commit -q -m "feat: update ublock-adv rules by ubk"
   git push -q
   echo "done."
   echo ""
-end
+else
+  echo "Error: No ublock backups in ~/Downloads"
+fi
