95c50426ccb2155b15bbcdbdbcd24accdf6b94a0
Florencea <bearflorencea@gmail.com>
2020-04-18 12:56:16 +0800

    feat: update brew list

--- install.sh
+++ install.sh
@@ -55,8 +55,8 @@ brew cask install iina
 brew cask install istat-menus
 brew cask install keka
 brew cask install kekadefaultapp
+brew cask install c0re100-qbittorrent
 brew cask install scroll-reverser
-brew cask install transmission
 
 print_step "npm install global packages"
 npm install -g http-server
