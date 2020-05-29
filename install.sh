62dcd23b05489f6be70fb1c22cf89889a1f0cc15
Florencea <bearflorencea@pm.me>
2020-05-29 20:19:08 +0800

    feat: udpate brew list

--- install.sh
+++ install.sh
@@ -53,6 +53,7 @@ brew tap homebrew/cask-versions
 brew cask install atom
 brew cask install firefox-nightly
 brew cask install iina
+brew cask install intel-power-gadget
 brew cask install istat-menus
 brew cask install keka
 brew cask install kekadefaultapp
@@ -62,6 +63,8 @@ brew cask install scroll-reverser
 print_step "brew install cask fonts"
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
+brew cask install font-fira-mono
+brew cask install font-inter
 
 print_step "npm install global packages"
 npm install -g http-server
