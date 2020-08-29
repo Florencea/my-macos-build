9ee8860cfaefc3dd6bdd6a60ea1192aa2583b8d1
Florencea <bearflorencea@gmail.com>
2020-08-29 11:33:44 +0800

    feat: update urb

---
+++ scripts/ublock-rule-backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+# nano ~/.config/fish/config.fish
+# alias urb="sh ~/GitHub/my-macos-build/ublock-rule-backup.sh"
+echo ""
+printf 'Minifiy ec-rules...'
+python3 ~/GitHub/my-macos-build/scripts/ublock-rule-combiner.py
+echo "done."
+echo ""
+printf 'Update ec-rules to github...'
+git add configs/*
+git commit -q -m "feat: update ec-rules by urb"
+git push -q
+echo "done."
+echo ""
