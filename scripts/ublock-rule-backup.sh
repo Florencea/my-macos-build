432a804173e875b53b18cad89f1a00a998171500
Florencea <bearflorencea@gmail.com>
2020-08-29 11:35:08 +0800

    feat: udpate urb

--- scripts/ublock-rule-backup.sh
+++ scripts/ublock-rule-backup.sh
@@ -7,7 +7,7 @@ python3 ~/GitHub/my-macos-build/scripts/ublock-rule-combiner.py
 echo "done."
 echo ""
 printf 'Update ec-rules to github...'
-git add configs/*
+git add ~/GitHub/my-macos-build/configs/*
 git commit -q -m "feat: update ec-rules by urb"
 git push -q
 echo "done."
