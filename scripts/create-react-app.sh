29f78e6a215a94ee50298af62b7da3aac10fcd57
Florencea <bearflorencea@gmail.com>
2020-12-15 09:22:10 +0800

    fix: display

--- scripts/create-react-app.sh
+++ scripts/create-react-app.sh
@@ -13,7 +13,7 @@ function print_help() {
 if [[ $# -eq 1 ]]; then
   APP_NAME=$1
   printf "\E[1;36m"
-  printf "\n <-+ Create React App Custom Script with Bootstrap +-->\n\n"
+  printf "\n <--+ Create React App Custom Script with Bootstrap +-->\n\n"
   printf "\E[0m"
   print_step "Run Create React App"
   (
