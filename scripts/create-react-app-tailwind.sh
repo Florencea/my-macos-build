7952e95fa3e9369b954a5b756916bf06ba11776a
Florencea <bearflorencea@gmail.com>
2020-12-17 10:26:18 +0800

    fix: typo

--- scripts/create-react-app-tailwind.sh
+++ scripts/create-react-app-tailwind.sh
@@ -2,7 +2,7 @@
 
 function print_step() {
   printf "\E[1;31m"
-  printf " +--> "
+  printf " + "
   printf "\E[0m"
   printf "%s\n" "$1"
 }
@@ -13,37 +13,35 @@ function print_help() {
 if [[ $# -eq 1 ]]; then
   APP_NAME=$1
   printf "\E[1;31m"
-  printf "\n <--+ Create React App Custom Script with Tailwind CSS +-->\n\n"
+  printf "\n + Create React App Custom Script with Tailwind CSS\n\n"
   printf "\E[0m"
   print_step "Run Create React App"
   (
-    # set -x
     yarn create react-app "$APP_NAME" --template typescript &>/dev/null
   )
   cd "$APP_NAME" || exit
-  print_step "Install Tailwind CSS, FontAwesome"
+  print_step "Add Tailwind CSS, FontAwesome"
   (
-    # set -x
     yarn add tailwindcss@npm:@tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9 @craco/craco @fortawesome/fontawesome-free &>/dev/null
   )
-  print_step "Install ESLint Plugins: React, JSX-a11y, Import, Promise, Node, Prettier"
+  print_step "Add ESLint Plugins: React, JSX-a11y, Import, Promise, Node, Prettier"
   (
-    # set -x
     yarn add eslint-plugin-react eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-promise eslint-plugin-node eslint-plugin-prettier --dev &>/dev/null
   )
   print_step "Run Google gts"
   (
-    # set -x
     npx gts init -y --yarn &>/dev/null
   )
-  print_step "Create Tailwind configuration file"
+  print_step "Create Tailwind configuration"
   (
-    # set x
+    # tailwind.config.js
     npx -y tailwindcss init &>/dev/null
+    # craco.config.js
+    echo 'module.exports = { style: { postcss: { plugins: [require("tailwindcss"), require("autoprefixer")], }, }, };' >>craco.config.js
   )
-  print_step "Rewrite package.json, tsconfig.json, eslintrc.json, reportWebVitals.ts .gitignore, tailwind.config.js, index.css"
+  print_step "Customize configurations"
   (
-    # set -x
+    # package.json
     jq '.scripts.start = "craco start"' <package.json >package2.json
     jq '.scripts.build = "craco build"' <package2.json >package3.json
     jq '.scripts.test = "craco test --coverage --verbose"' <package3.json >package4.json
@@ -53,76 +51,52 @@ if [[ $# -eq 1 ]]; then
     rm package3.json
     rm package4.json
     mv package5.json package.json
-  )
-  (
-    # set -x
+    # tsconfig.json
     echo '{"extends":"./node_modules/gts/tsconfig-google.json","compilerOptions":{"rootDir":".","outDir":"build","target":"es5","lib":["dom","dom.iterable","esnext"],"allowJs":true,"skipLibCheck":true,"esModuleInterop":true,"allowSyntheticDefaultImports":true,"strict":true,"forceConsistentCasingInFileNames":true,"noFallthroughCasesInSwitch":true,"module":"esnext","moduleResolution":"node","resolveJsonModule":true,"isolatedModules":true,"noEmit":true,"jsx":"react-jsx"},"include":["src"]}' >tsconfig.json
-  )
-  (
-    # set -x
+    # eslintrc.json
     echo '{"extends":["./node_modules/gts/","plugin:react/recommended","plugin:jsx-a11y/recommended","plugin:import/errors","plugin:import/warnings","plugin:import/typescript","plugin:promise/recommended"],"env":{"browser":true,"jest":true},"plugins":["react","jsx-a11y","import","promise"],"settings":{"react":{"version":"detect"}}}' >.eslintrc2.json
     rm .eslintrc.json
     mv .eslintrc2.json .eslintrc.json
-  )
-  (
-    # set -x
+    # src/reportWebVitals.ts
     mv src/reportWebVitals.ts src/reportWebVitals.old.ts
     echo 'import { ReportHandler } from "web-vitals";const reportWebVitals = (onPerfEntry?: ReportHandler) => { if (onPerfEntry && onPerfEntry instanceof Function) { import("web-vitals") .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => { getCLS(onPerfEntry); getFID(onPerfEntry); getFCP(onPerfEntry); getLCP(onPerfEntry); getTTFB(onPerfEntry); return; }) .catch((err) => { console.error(err); }); } };export default reportWebVitals;' >>src/reportWebVitals.ts
     rm src/reportWebVitals.old.ts
-  )
-  (
-    # set -x
+    # .gitignore
     printf "\n# lint\n.eslintcache\n" >>.gitignore
-  )
-  (
-    # set -x
+    # tailwind.config.js
     mv tailwind.config.js tailwind.config.old.js
     echo 'module.exports = { purge: ["./src/**/*.js", "./public/index.html"], darkMode: false, theme: { extend: {}, }, variants: {}, plugins: [], };' >tailwind.config.js
     rm tailwind.config.old.js
-  )
-  (
-    # set -x
+    # src/index.css
     mv src/index.css src/index.old.css
     printf "@tailwind base;\n@tailwind components;\n@tailwind utilities;\n" >src/index.css
     rm src/index.old.css
-  )
-  print_step "Remove conflict devlopment dependencies: typescript, @types/node"
-  (
-    # set -x
-    yarn remove typescript @types/node --dev &>/dev/null
-    yarn add typescript @types/node --dev &>/dev/null
-  )
-  print_step "Add FontAwesome to src/App.tsx"
-  (
-    # set -x
+    # src/App.tsx
     awk '/logo.svg/ { print; print "import '\''@fortawesome/fontawesome-free/css/all.min.css'\'';"; next }1' src/App.tsx >src/App2.tsx
     rm src/App.tsx
     mv src/App2.tsx src/App.tsx
   )
-  print_step "Create craco.config.js"
+  print_step "Remove conflicts: typescript, @types/node"
   (
-    # set -x
-    echo 'module.exports = { style: { postcss: { plugins: [require("tailwindcss"), require("autoprefixer")], }, }, };' >>craco.config.js
+    yarn remove typescript @types/node --dev &>/dev/null
+    yarn add typescript @types/node --dev &>/dev/null
   )
   print_step "Run yarn fix"
   (
-    # set -x
     yarn fix &>/dev/null
   )
-  print_step "Add commit for modefied files"
+  print_step "Commit modefied files"
   (
-    # set -x
     git config advice.addIgnoredFile false
-    git add ./.gitignore
-    git add ./*
-    git add ./.eslintignore
-    git add ./.eslintrc.json
-    git add ./.prettierrc.js
-    git commit -q -m "init: Initialize project using customized Create React App script"
+    git add ./.gitignore &>/dev/null
+    git add ./* &>/dev/null
+    git add ./.eslintignore &>/dev/null
+    git add ./.eslintrc.json &>/dev/null
+    git add ./.prettierrc.js &>/dev/null
+    git commit -q -m "init: Initialize project using customized Create React App script" &>/dev/null
   )
-  print_step "Open VSCode for $APP_NAME"
+  print_step "Done."
   (
-    # set -x
     code -n .
   )
 else
