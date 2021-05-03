996e0dc2760d2dc44d7423265095a599531ead6d
Florencea <bearflorencea@gmail.com>
2021-05-03 09:26:02 +0800

    feat: add git-fetch-all.sh

---
+++ scripts/git-fetch-all.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+printf "\nUpdate all Repositories in ~/GitHub\n"
+BASE_DIR="/Users/$(whoami)/GitHub/"
+cd "$BASE_DIR"
+for f in $(ls $BASE_DIR); do
+  cd "$BASE_DIR/$f"
+  printf "+ git pull: %s\n" $f
+  git pull
+done
+
+printf "\nUpdate all Repositories in ~/BOX\n"
+BASE_DIR="/Users/$(whoami)/BOX/"
+cd "$BASE_DIR"
+for f in $(ls $BASE_DIR); do
+  if [ "$f" != "_data" ]; then
+    cd "$BASE_DIR/$f"
+    printf "+ git pull: %s\n" $f
+    git pull
+  fi
+done
