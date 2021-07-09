5c72f44048bcfaff6d87a6694df8bc04754f0bec
Florencea <bearflorencea@gmail.com>
2021-07-09 17:06:16 +0800

    feat: update fa

--- scripts/git-fetch-all.sh
+++ scripts/git-fetch-all.sh
@@ -12,7 +12,7 @@ printf "\nUpdate all Repositories in ~/BOX\n"
 BASE_DIR="/Users/$(whoami)/BOX/"
 cd "$BASE_DIR"
 for f in $(ls $BASE_DIR); do
-  if [ "$f" != "_data" ]; then
+  if [ "$f" != "_data" ] && [ "$f" != "_data_local" ]; then
     cd "$BASE_DIR/$f"
     printf "+ git pull: %s\n" $f
     git pull
