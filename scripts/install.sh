98e8c1e676442d5c920d2d9c5ef4edaf615e22d7
Florencea <bearflorencea@gmail.com>
2021-09-10 21:49:43 +0800

    feat: update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -56,6 +56,7 @@ brew install font-inter
 #   printf "alias ua=\"sh ~/GitHub/my-macos-build/scripts/update-all.sh\"\n"
 #   printf "alias urb=\"sh ~/GitHub/my-macos-build/scripts/ublock-rule-backup.sh\"\n"
 #   printf "alias fa=\"sh ~/GitHub/my-macos-build/scripts/git-fetch-all.sh\"\n"
+#   printf "alias el=\"exa -lgh --icons --time-style long-iso\"\n"
 # } >>~/.config/fish/config.fish
 
 print_step "brew install fish"
@@ -73,7 +74,7 @@ mkdir -p ~/.config/fish
   printf "alias ua=\"sh ~/GitHub/my-macos-build/scripts/update-all.sh\"\n"
   printf "alias urb=\"sh ~/GitHub/my-macos-build/scripts/ublock-rule-backup.sh\"\n"
   printf "alias fa=\"sh ~/GitHub/my-macos-build/scripts/git-fetch-all.sh\"\n"
-  printf "alias el=\"exa -lgh --time-style long-iso\"\n"
+  printf "alias el=\"exa -lgh --icons --time-style long-iso\"\n"
 } >>~/.config/fish/config.fish
 
 print_step "brew install cask apps"
