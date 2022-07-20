55e628a4eafbad453f50de1e2429a13f26dedb5b
Florencea <bearflorencea@gmail.com>
2022-07-20 14:09:48 +0800

    feat(break): remove all config and rules

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,8 +40,6 @@ mkdir -p ~/.config/fish
   printf "set -gx fish_greeting\n"
   printf "alias mmb=\"code $script_dir\"\n"
   printf "alias mkgif=\"sh $script_path/make-gif.sh\"\n"
-  printf "alias ebk=\"sh $script_path/extension-config-backup.sh\"\n"
-  printf "alias urb=\"sh $script_path/ublock-rule-backup.sh\"\n"
   printf "alias ua=\"sh $script_path/update-all.sh\"\n"
   printf "alias rec=\"sh $script_path/re-encode.sh\"\n"
   printf "alias rsl=\"defaults write com.apple.dock ResetLaunchPad -bool true;killall Dock\"\n"
