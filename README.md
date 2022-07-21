ba2185330f3b15cce82d99b8f918cf3b35eb8196
Florencea <bearflorencea@gmail.com>
2022-07-21 21:21:33 +0800

    revert: all settings back

--- README.md
+++ README.md
@@ -6,3 +6,51 @@
 - [Create React App Note](NOTE_CRA.md)
 - [Next.js Note](NOTE_NEXTJS.md)
 - [FFmpeg Note](NOTE_FFMPEG.md)
+- [Iceraven Note](NOTE_ICERAVEN.md)
+
+## Extenstion Configs
+
+- [Adguard Configs](https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/adg-settings.json)
+- [uBlock Origin Configs](https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/ublock-advanced.txt)
+- [Violentmonkey Configs](https://github.com/Florencea/my-macos-build/raw/main/configs/violentmonkey-backup.zip)
+- [NewTongwentong Configs](https://github.com/Florencea/my-macos-build/raw/main/configs/tongwentang-pref.json)
+
+## macOS Setup
+
+- For Apple Silicon and macOS 12
+
+### 1. Logout Browsers and Reinstall macOS
+
+- <https://support.apple.com/zh-tw/HT212749>
+
+### 2. System Performance
+
+- Privacy -> Full Disk Access, Add "Terminal.app"
+
+### 3. Finder and Dock
+
+### 4. Data Recovery from Backup
+
+- Setup "Music.app"
+- Import media files to "Music.app"
+- Copy data to ~/
+
+### 5. Install Apps from App Store
+
+### 6. Execute install.sh
+
+### 7. Setup Apps
+
+## Firefox Devtools
+
+```text
+Mozilla/5.0 (Android 12; Mobile; rv:102.0) Gecko/102.0 Firefox/102.0
+```
+
+```text
+396 x 858
+```
+
+```text
+2.727272727272727
+```
---
+++ scripts/extension-config-backup.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+# nano ~/.config/fish/config.fish
+# alias ebk="sh ~/Codespaces/my-macos-build/scripts/extension-config-backup.sh"
+FILE_ADGUARD=$(find ~/Downloads -maxdepth 1 -name '*_adg_ext_settings_*.json' | head -n1)
+FILE_ADGUARD_NAME=adg-settings.json
+FILE_UBLOCK=$(find ~/Downloads -maxdepth 1 -name 'my-ublock-backup*.txt' | head -n1)
+FILE_UBLOCK_NAME=ublock-advanced.txt
+FILE_VIOLENTMONKEY=$(find ~/Downloads -maxdepth 1 -name 'scripts_*.zip' | head -n1)
+FILE_VIOLENTMONKEY_NAME=violentmonkey-backup.zip
+FILE_TONGWENTANG=$(find ~/Downloads -maxdepth 1 -name 'tongwentang-pref*.json' | head -n1)
+FILE_TONGWENTANG_NAME=tongwentang-pref.json
+PROJECT_DIR=~/Codespaces/my-macos-build/configs/
+# find *_adg_ext_settings_*.json and backup
+if [ -f "$FILE_ADGUARD" ]; then
+  mv "$FILE_ADGUARD" "$PROJECT_DIR$FILE_ADGUARD_NAME"
+  cd $PROJECT_DIR || exit
+  git add $FILE_ADGUARD_NAME
+  echo ""
+  printf 'Find AdGuard Configuration: %s\n' "$FILE_ADGUARD"
+  printf '     --> adg-settings.json\n\n'
+  printf 'Backup...'
+  git commit -q -m "feat: Update AdGuard Configuration by ebk"
+  git push -q
+  echo "done."
+  echo ""
+fi
+# find my-ublock-backup*.txt and backup
+if [ -f "$FILE_UBLOCK" ]; then
+  mv "$FILE_UBLOCK" "$PROJECT_DIR$FILE_UBLOCK_NAME"
+  cd $PROJECT_DIR || exit
+  git add $FILE_UBLOCK_NAME
+  echo ""
+  printf 'Find uBlock Configuration: %s\n' "$FILE_UBLOCK"
+  printf '     --> ublock-advanced.txt\n\n'
+  printf 'Backup...'
+  git commit -q -m "feat: Update uBlock Configuration by ebk"
+  git push -q
+  echo "done."
+  echo ""
+fi
+# find scripts_*.zip and backup
+if [ -f "$FILE_VIOLENTMONKEY" ]; then
+  mv "$FILE_VIOLENTMONKEY" "$PROJECT_DIR$FILE_VIOLENTMONKEY_NAME"
+  cd $PROJECT_DIR || exit
+  git add $FILE_VIOLENTMONKEY_NAME
+  echo ""
+  printf 'Find VIOLENTMONKEY Configuration: %s\n' "$FILE_VIOLENTMONKEY"
+  printf '     --> violentmonkey-backup.zip\n\n'
+  printf 'Backup...'
+  git commit -q -m "feat: Update violentmonkey Configuration by ebk"
+  git push -q
+  echo "done."
+  echo ""
+fi
+# find tongwentang-pref*.json and backup
+if [ -f "$FILE_TONGWENTANG" ]; then
+  mv "$FILE_TONGWENTANG" "$PROJECT_DIR$FILE_TONGWENTANG_NAME"
+  cd $PROJECT_DIR || exit
+  git add $FILE_TONGWENTANG_NAME
+  echo ""
+  printf 'Find NewTongWenTang Configuration: %s\n' "$FILE_TONGWENTANG"
+  printf '     --> tongwentang-pref.json\n\n'
+  printf 'Backup...'
+  git commit -q -m "feat: Update NewTongWenTang Configuration by ebk"
+  git push -q
+  echo "done."
+  echo ""
+fi
