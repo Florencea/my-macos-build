9fa3383dc0554bb87f1846cb0d4f4943b77658b6
Florencea <bearflorencea@gmail.com>
2022-08-05 14:27:54 +0800

    feat: add adg

--- scripts/ublock-rule-backup.sh
+++ scripts/ublock-rule-backup.sh
@@ -5,15 +5,21 @@ echo ""
 printf 'Minifiy ec-rules for ubo...'
 python3 ~/Codespaces/my-macos-build/scripts/ublock-rule-combiner.py
 echo "done."
+printf 'Minifiy ec-rules for adg...'
+python3 ~/Codespaces/my-macos-build/scripts/adguard-rule-combiner.py
+echo "done."
 echo ""
 printf 'Update ec-rules...'
 cd ~/Codespaces/my-macos-build/ || exit
 git add configs/element-custom-rules-desktop.txt
 git add configs/element-custom-rules-desktop.combined.txt
+git add configs/element-custom-rules-desktop.adguard.txt
 git add configs/element-custom-rules-mobile.txt
 git add configs/element-custom-rules-mobile.combined.txt
+git add configs/element-custom-rules-mobile.adguard.txt
 git add configs/element-custom-rules-dark.txt
 git add configs/element-custom-rules-dark.combined.txt
+git add configs/element-custom-rules-dark.adguard.txt
 git commit -q -m "feat: update ec-rules by urb"
 git push -q
 echo "done."
