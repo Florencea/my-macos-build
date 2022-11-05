622fa7d3c5af8520656a3538da5c0ca45c035bc9
Florencea <bearflorencea@gmail.com>
2022-11-05 20:39:25 +0800

    feat: update ua script

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -4,32 +4,27 @@
 
 function print_step() {
   printf "\E[1;36m"
-  printf "\n + %s\n\n" "$1"
+  printf "\n+ %s\n\n" "$1"
   printf "\E[0m"
 }
 
-print_step "Update brew cli and apps"
+print_step "Update brew cli, apps and npm"
 cd ~ || exit
-(
-  set -x
-  brew upgrade
-)
+printf "> brew upgrade\n\n"
+brew upgrade
+printf "> npm@"
+npm --version
+npm install --loglevel=silent --global npm
+printf "  -> npm@"
+npm --version
 
-print_step "Update global npm packages"
-cd ~ || exit
-(
-  set -x
-  npm update --global
-  npm list --global
-)
-
-print_step "Update all Repositories in ~/Codespaces"
 BASE_DIR="/Users/$(whoami)/Codespaces/"
-cd "$BASE_DIR"
-for f in $(ls $BASE_DIR); do
-  cd "$BASE_DIR/$f"
+print_step "Update all Repositories in $BASE_DIR"
+cd "$BASE_DIR" || exit
+for dir in $(ls $BASE_DIR); do
+  cd "$BASE_DIR/$dir"
   if [ -d .git ]; then
-    printf "+ git pull --all: %s\n" $f
+    printf "> %s\n" $dir
     git pull --all
   fi
 done
