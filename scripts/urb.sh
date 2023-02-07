c0745e885fd68a75f26c6e1341239102a85d33b7
Florencea <bearflorencea@gmail.com>
2023-02-07 16:28:55 +0800

    fix: ubo

--- scripts/urb.sh
+++ scripts/urb.sh
@@ -5,16 +5,16 @@ CONFIG_HOME=$2
 
 FILE_LIST="ubo-desktop ubo-mobile ubo-dark"
 
-printf 'Update ec-rules...'
+printf 'Update ubo-rules...'
 
 for FILE in ${FILE_LIST}; do
   cd "$HOME"
-  python3 "$SCRIPT_HOME/ublock-rule-combiner.py" "$CONFIG_HOME/$FILE.txt"
+  python3 "$SCRIPT_HOME/urb.py" "$CONFIG_HOME/$FILE.txt"
   cd "$CONFIG_HOME" || exit
   git add "$FILE.txt"
   git add "$FILE.min.txt"
 done
-git commit -q -m "feat: update ec-rules by urb"
+git commit -q -m "feat: update ubo-rules by urb"
 git push -q
 
 echo "done."
