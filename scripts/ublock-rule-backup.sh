4768a7809fe61190431472483225434933d77b75
Florencea <bearflorencea@gmail.com>
2023-02-07 15:16:03 +0800

    feat: update ebk

--- scripts/ublock-rule-backup.sh
+++ scripts/ublock-rule-backup.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
-# nano ~/.config/fish/config.fish
-# alias urb="sh ~/Codespaces/my-macos-build/scripts/ublock-rule-backup.sh"
+
 echo ""
+
 printf 'Minifiy ec-rules for ubo...'
 python3 ~/Codespaces/my-macos-build/scripts/ublock-rule-combiner.py
+
 echo "done."
 echo ""
+
 printf 'Update ec-rules...'
+
 cd ~/Codespaces/my-macos-build/ || exit
+
 git add configs/element-custom-rules-desktop.txt
 git add configs/element-custom-rules-desktop.combined.txt
 git add configs/element-custom-rules-mobile.txt
@@ -16,5 +20,6 @@ git add configs/element-custom-rules-dark.txt
 git add configs/element-custom-rules-dark.combined.txt
 git commit -q -m "feat: update ec-rules by urb"
 git push -q
+
 echo "done."
 echo ""
