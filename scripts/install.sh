aa1f65b5d2a11995a204ca181bd63fc155bb2e8c
Florencea <bearflorencea@gmail.com>
2023-06-11 18:09:09 +0800

    revert: use node@18

--- scripts/install.sh
+++ scripts/install.sh
@@ -90,7 +90,7 @@ brew install mtr
 brew install nano
 brew install nanorc
 echo "include /opt/homebrew/share/nanorc/*.nanorc" >~/.nanorc
-brew install node@20
+brew install node@18
 printf "audit=false\nfund=false\nloglevel=error\nupdate-notifier=false\nengine-strict=true" >~/.npmrc
 brew install rsync
 brew install python
