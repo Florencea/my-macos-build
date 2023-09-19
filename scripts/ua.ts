ea30c838b824d41061e74d3aac33d47b6317da5f
Florencea <bearflorencea@gmail.com>
2023-09-20 06:29:58 +0800

    feat: make ua.ts nodejs, deno compatible

--- scripts/ua.ts
+++ scripts/ua.ts
@@ -1,52 +1,30 @@
-import { readdir } from "node:fs/promises";
+import { spawn, spawnSync } from "node:child_process";
+import { existsSync, readdirSync } from "node:fs";
 import { homedir } from "node:os";
 import { join } from "node:path";
-
-/**
- * Check if file or directory exist
- * @param path File/Dir path
- * @returns if file or directory exist
- */
-const isInodeExist = async (path: string) => {
-  const file = Bun.file(path);
-  return file.size > 0;
-};
-
-/**
- * Single line logger
- * @param content content to print
- * @param withNewline print `\n` at content end, default: `false`
- */
-const logger = async (content: string, withNewline: boolean = false) => {
-  await Bun.write(Bun.stdout, `${content}${withNewline ? "\n" : ""}`);
-};
+import { argv, stdout } from "node:process";
 
 /**
  * Sync git projects
  * @param codeSpacePath codespace path
  */
-const syncGitProjects = async (codeSpacePath: string) => {
-  (await readdir(codeSpacePath, { withFileTypes: true }))
+const syncGitProjects = (codeSpacePath: string) => {
+  readdirSync(codeSpacePath, { withFileTypes: true })
     .filter((inode) => inode.isDirectory())
     .map((dir) => dir.name)
-    .map(async (projectDir) => {
+    .map((projectDir) => {
       const cwd = join(codeSpacePath, projectDir);
       const gitDir = join(cwd, ".git");
-      if (await isInodeExist(gitDir)) {
-        const p = Bun.spawn({
+      if (existsSync(gitDir)) {
+        const p = spawn("git", ["pull", "--all"], {
           cwd,
-          cmd: ["git", "pull", "--all"],
-          stdout: "pipe",
-          stderr: "pipe",
         });
-        const stdoutText = await new Response(p.stdout).text();
-        const stderrText = await new Response(p.stderr).text();
-        if (stdoutText) {
-          await logger(`Syncing \x1b[1m${projectDir}\x1b[0m ${stdoutText}`);
-        }
-        if (stderrText) {
-          await logger(`Syncing \x1b[1m${projectDir}\x1b[0m \n${stderrText}`);
-        }
+        p.stdout.on("data", (data) => {
+          stdout.write(`Syncing \x1b[1m${projectDir}\x1b[0m ${data}`);
+        });
+        p.stderr.on("data", (data) => {
+          stdout.write(`Syncing \x1b[1m${projectDir}\x1b[0m \n${data}`);
+        });
       }
     });
 };
@@ -54,20 +32,16 @@ const syncGitProjects = async (codeSpacePath: string) => {
 /**
  * brew upgrade
  */
-const brewUpgrade = () =>
-  Bun.spawnSync({
-    cwd: homedir(),
-    cmd: ["brew", "upgrade"],
-    stdout: "inherit",
-    stderr: "inherit",
-  });
+const brewUpgrade = () => {
+  spawnSync("brew", ["upgrade"], { cwd: homedir() });
+};
 
 /**
  * main function
  */
-const main = async () => {
+const main = () => {
   brewUpgrade();
-  await syncGitProjects(Bun.argv[2]);
+  syncGitProjects(argv[2]);
 };
 
 main();
