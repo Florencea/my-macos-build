6cd38629dcb3a81b678cf46f4a982ce377e7526a
Florencea <bearflorencea@gmail.com>
2023-09-29 01:56:38 +0800

    feat: port clall to cli

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
-#! /bin/bash
+#! /usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
 
+### Use Touch ID for sudo Commands
 if ! (cat /etc/pam.d/sudo | grep 'pam_tid.so'); then
   echo "Set TouchID for sudo commands"
   sudo sed -i '' '2i\
@@ -9,6 +13,7 @@ else
   echo "File modefied, skip Set TouchID for sudo commands"
 fi
 
+### Install Homebrew
 if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
@@ -17,9 +22,13 @@ else
   echo "Homebrew exist, skip Homebrew installation"
 fi
 
+### Setup SFMono fonts
 cp -R /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/*.otf ~/Library/Fonts/
+### Disable Window Animations
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
+### Disable macOS popup showing accented characters when holding down a key
 defaults write -g ApplePressAndHoldEnabled -bool false
+### Setup git config
 git config --global user.name "Florencea"
 git config --global user.email "bearflorencea@gmail.com"
 git config --global core.editor "nano"
@@ -30,6 +39,7 @@ git config --global core.ignorecase false
 git config --global diff.lockb.textconv bun
 git config --global diff.lockb.binary true
 
+### Generate SSH Key
 if [ -d "$HOME/.ssh" ]; then
   echo "$HOME/.ssh exist, skip ssh key generation"
 else
@@ -37,23 +47,28 @@ else
   ssh-keygen -q -t ed25519 -N '' -f "$HOME/.ssh/id_ed25519" && cat "$HOME/.ssh/id_ed25519.pub"
 fi
 
+### Update Homebrew taps
 brew tap homebrew/cask-versions
 brew tap homebrew/cask-fonts
 
+### Install essential fonts
 brew install --cask font-jetbrains-mono
 brew install --cask font-inter
 
+### Install zsh
 brew install zsh
 brew install zsh-autosuggestions
 brew install zsh-fast-syntax-highlighting
 curl -fsSL https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/zshrc.txt -o "$HOME/.zshrc"
 
+### Install fish
 brew install fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 chsh -s /opt/homebrew/bin/fish
 mkdir -p "$HOME/.config/fish"
 curl -fsSL https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/config.fish.txt -o "$HOME/.config/fish/config.fish"
 
+### Install apps
 brew install --cask istat-menus
 defaults write com.bjango.istatmenus license6 -dict email "982092332@qq.com" serial "GAWAE-FCWQ3-P8NYB-C7GF7-NEDRT-Q5DTB-MFZG6-6NEQC-CRMUD-8MZ2K-66SRB-SU8EW-EDLZ9-TGH3S-8SGA"
 brew install --cask google-chrome
@@ -64,6 +79,7 @@ brew install --cask mos
 brew install --cask c0re100-qbittorrent
 brew install --cask visual-studio-code
 
+### Install cli tools
 brew install bash
 brew tap oven-sh/bun
 brew install bun
@@ -91,6 +107,7 @@ brew install wget
 brew install yt-dlp/taps/yt-dlp
 brew install yq
 
+### Install colima
 brew install colima
 brew install docker
 brew install docker-compose
@@ -101,6 +118,7 @@ else
   yq -i '.cpu=8 | .memory=8 | .arch="aarch64" | .network.dns|=["8.8.8.8", "8.8.4.4"] | .vmType="vz" | .rosetta=true | .mountType="virtiofs"' "$HOME/.colima/_templates/default.yaml"
 fi
 
+### Install rust toolchain
 if ! command -v rustup-init &>/dev/null; then
   brew install rustup-init
   rustup-init -y --no-modify-path
@@ -108,8 +126,11 @@ else
   echo "rustup-init exist, skip Rust installation"
 fi
 
+### Install Rosetta2
 /usr/bin/pgrep -q oahd && echo "rosetta2 exist, skip installation" || /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
+### Reset LaunchPad
 defaults write com.apple.dock ResetLaunchPad -bool true
 killall Dock
+### Clear script
 rm "$0"
