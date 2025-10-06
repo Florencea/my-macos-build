6804c355e3907d6db5cca806a73284b734ba9a7c
Florencea <bearflorencea@gmail.com>
2025-10-06 19:49:44 +0800

    feat: add zed config

--- scripts/install.sh
+++ scripts/install.sh
@@ -65,7 +65,8 @@ brew install --cask keka
 brew install --cask kekaexternalhelper
 brew install --cask logi-options+
 brew install --cask tailscale
-brew install --cask visual-studio-code
+brew install --cask zed
+curl -fsSL https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/settings.json -o "$HOME/.config/zed/settings.json"
 
 ### Rosetta2
 /usr/sbin/softwareupdate --install-rosetta --agree-to-license
