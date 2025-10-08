457aea3817101a6afc8615147f71eb6251cda89d
Florencea <bearflorencea@gmail.com>
2025-10-08 10:07:54 +0800

    fix: mkdir before copy configs

--- scripts/install.sh
+++ scripts/install.sh
@@ -66,6 +66,7 @@ brew install --cask kekaexternalhelper
 brew install --cask logi-options+
 brew install --cask tailscale
 brew install --cask zed
+mkdir -p "$HOME/.config/zed"
 curl -fsSL https://raw.githubusercontent.com/Florencea/my-macos-build/main/configs/settings.json -o "$HOME/.config/zed/settings.json"
 
 ### Rosetta2
