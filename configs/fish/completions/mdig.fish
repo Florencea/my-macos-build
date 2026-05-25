41b4637e91d7097408b5a87cb2a4b1a9277db2ea
Florencea <bearflorencea@gmail.com>
2026-05-25 17:15:57 +0800

    refactor(cli): merge mdig and mdig6, separate A/AAAA grouping with dual-stack queries

--- configs/fish/completions/mdig.fish
+++ configs/fish/completions/mdig.fish
@@ -1,3 +1,3 @@
 complete -c mdig -e
 complete -c mdig -f
-complete -c mdig -d 'Multi-dig: Check A records on different DNS server at once.'
+complete -c mdig -d 'Multi-dig: Check both A and AAAA records on different DNS servers at once.'
--- configs/fish/completions/mdig6.fish
+++
@@ -1,3 +0,0 @@
-complete -c mdig6 -e
-complete -c mdig6 -f
-complete -c mdig6 -d 'Multi-dig-ipv6: Check AAAA records on different DNS server at once.'
