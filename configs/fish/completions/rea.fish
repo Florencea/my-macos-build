a4e52936f73df5099c123dfab0923f5b5b569fa6
Florencea <bearflorencea@gmail.com>
2026-05-31 13:24:15 +0800

    feat(cli): improve rea script to package macOS compatible MP4

--- configs/fish/completions/rea.fish
+++ configs/fish/completions/rea.fish
@@ -1,4 +1,4 @@
 complete -c rea -e
 complete -c rea -f
 complete -c rea -n '__fish_is_first_arg' -a '(__fish_complete_directories)'
-complete -c rea -d 'ASS-Combiner: combile .cht.ass to mkv'
+complete -c rea -d 'ASS-Combiner: combine .cht.ass to macOS-compatible mp4'
