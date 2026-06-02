5c7a64938f5d3090672ba0e9a892c28b2331b14d
Florencea <bearflorencea@gmail.com>
2026-06-03 02:56:25 +0800

    refactor: merge mkgifv into mkgif, support --fps parameter and unify naming convention

--- configs/fish/completions/mkgif.fish
+++ configs/fish/completions/mkgif.fish
@@ -1,4 +1,5 @@
 complete -c mkgif -e
 complete -c mkgif -f
 complete -c mkgif -n '__fish_is_first_arg' -F
-complete -c mkgif -d 'Gif Maker (12fps, 480p, q100)'
+complete -c mkgif -l fps -r -d 'Set FPS (default: 12)'
+complete -c mkgif -d 'Gif Maker (480p, q100)'
--- configs/fish/completions/mkgifv.fish
+++
@@ -1,4 +0,0 @@
-complete -c mkgifv -e
-complete -c mkgifv -f
-complete -c mkgifv -n '__fish_is_first_arg' -F
-complete -c mkgifv -d 'Gif Maker (24fps, 480p, q100)'
