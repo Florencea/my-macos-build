0067706cbac106f7a1fc61465adc1129d923bd7d
Florencea <bearflorencea@gmail.com>
2026-06-03 02:59:59 +0800

    completion(mkgif): restrict --fps autocomplete to numbers and exclude files

--- configs/fish/completions/mkgif.fish
+++ configs/fish/completions/mkgif.fish
@@ -1,5 +1,5 @@
 complete -c mkgif -e
 complete -c mkgif -f
 complete -c mkgif -n '__fish_is_first_arg' -F
-complete -c mkgif -l fps -r -d 'Set FPS (default: 12)'
+complete -c mkgif -l fps -x -a '12 15 24 30 60' -d 'Set FPS (default: 12)'
 complete -c mkgif -d 'Gif Maker (480p, q100)'
