67813801757cad48bbc122343a642133128bffee
Florencea <bearflorencea@gmail.com>
2026-07-24 08:14:50 +0800

    refactor(cli): update rea script to directly mux .cht.ass into mkv as default subtitle

--- configs/fish/completions/rea.fish
+++ configs/fish/completions/rea.fish
@@ -1,4 +1,4 @@
 complete -c rea -e
 complete -c rea -f
 complete -c rea -n '__fish_is_first_arg' -a '(__fish_complete_directories)'
-complete -c rea -d 'ASS-Combiner: combine .cht.ass to macOS-compatible mp4, move up, and clean up directory'
+complete -c rea -d 'ASS-Combiner: combine .cht.ass into original mkv as default subtitle track, move up, and clean up directory'
