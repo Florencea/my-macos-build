a08928cd78719024e9ec9ee7f0a1719739ec761d
Florencea <bearflorencea@gmail.com>
2026-08-05 18:22:25 +0800

    chore: replace microsoft-edge with google-chrome in brew install list

--- scripts/install.sh
+++ scripts/install.sh
@@ -75,7 +75,7 @@ defaults write com.bjango.istatmenus license6 -dict email "982092332@qq.com" ser
 brew install --cask \
   logi-options+ \
   1password \
-  microsoft-edge \
+  google-chrome \
   keka \
   kekaexternalhelper \
   iina \
