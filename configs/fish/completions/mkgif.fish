a0678f16fe2b9852def9be40a0ac9c6bab17f425
Florencea <bearflorencea@gmail.com>
2026-09-10 23:18:49 +0800

    feat(cli): upgrade mkgif with width/quality options, 720w default, and unrestricted yt-dlp formats

--- configs/fish/completions/mkgif.fish
+++ configs/fish/completions/mkgif.fish
@@ -1,5 +1,8 @@
 complete -c mkgif -e
 complete -c mkgif -f
 complete -c mkgif -n '__fish_is_first_arg' -F
-complete -c mkgif -l fps -x -a '12 15 24 30 60' -d 'Set FPS (default: 12)'
-complete -c mkgif -d 'Gif Maker (480p, q100)'
+complete -c mkgif -l fps -x -a '8 10 12 15 24' -d 'Set FPS (default: 10)'
+complete -c mkgif -s w -l width -x -a '480 640 720' -d 'Set width in px (default: 720)'
+complete -c mkgif -s q -l quality -x -a '60 70 80 85' -d 'Set gifski quality 1-100 (default: 70)'
+complete -c mkgif -s l -l lossy -d 'Enable lossy LZW compression (default: off)'
+complete -c mkgif -d 'High-quality Gif Maker via gifski'
